1c6ef84df72bf15ecaaed828d20c106b55670436
Lorena Rincon <lorec.rincon@gmail.com>
2023-06-06 19:02:01 -0300

    Profile, Header, Buttons, Styles

---
+++ src/components/ProfileHeader.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import FollowButton from "./FollowButton";
+
+function ProfileHeader() {
+  return (
+    <div>
+      <div id="mainHeader">
+        <div id="profileHeader" className="row position-relative">
+          <div id="containerPhoto">
+            <img
+              id="profilePhoto"
+              src="/public/public copy PROV/img/foto_prueba.jpg"
+              className="rounded-circle"
+              width="170px"
+              height="170px"
+              alt="User profile photo"
+            />
+          </div>
+        </div>
+        <div id="profileInfo" className="row border position-relative">
+          <div className="col-2">
+            <FollowButton />
+          </div>
+          <div className="row p-0 m-0">
+            <div className="col-6 align-self-end">
+              <div id="userName" className="ms-2">
+                <h2 className="fs-4 m-0">Nombre de Usuario</h2>
+                <p className="fw-light m-0">@Nombredeusuario</p>
+              </div>
+            </div>
+            <div className="col-6 col-md-4 text-end align-self-end">
+              <div className="d-flex justify-content-between">
+                <a className="text-decoration-none text-black" href="#">
+                  <p className="m-0 d-inline-block">
+                    <strong className="text-black">123</strong>
+                    Followers
+                  </p>
+                </a>
+                <a className="text-decoration-none text-black" href="#">
+                  <p className="m-0 d-inline-block">
+                    <strong className="text-black">123</strong>
+                    Following
+                  </p>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="row p-0 m-0">
+            <div className="col-12 align-self-end">
+              <h4 className="m-0" id="tweets">
+                Tweets
+              </h4>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ProfileHeader;
