220693cda2ad3b54e1b0a5dd7df2485b8dee15ec
estebanCastaneira <estebanc_b@hotmail.com>
2023-06-06 17:36:32 -0300

    ultimos detalles

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
+  {/* login
+    register */}
 
+    {/* home */}
 
+    {/* profile */}
 
+    {/* following
+    followers */}
 
+    {/* 404 */}
+    
+    {/* tweet
+    tweetform */}
 
-
-
-
-
-
-
-
+{/* navBar
+    sidebar */}
 
 import './App.css'
 
