7a48bf09cb881a624a762ffb4a04cdee418d268b
valentinalr <valentina.lerose@outlook.com>
2023-06-06 20:58:31 -0300

    storeConfig

--- src/redux/storeConfig.js
+++ src/redux/storeConfig.js
@@ -1,6 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
 
-const store = configureStore({});
+const store = configureStore({
+  reducer: {
+    user: userReducer,
+  },
+});
 
 export default store;
