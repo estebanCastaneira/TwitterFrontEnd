0107173fdf7c11f79f450129c0f303286564b0c1
Lorena Rincon <lorec.rincon@gmail.com>
2023-06-07 13:02:48 -0300

    Profile with Tweet

--- src/components/ProfileHeader.jsx
+++ src/components/ProfileHeader.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import FollowButton from "./FollowButton";
+import axios from "axios";
 
 function ProfileHeader() {
   return (
@@ -21,24 +22,24 @@ function ProfileHeader() {
           <div className="col-2">
             <FollowButton />
           </div>
-          <div className="row p-0 m-0">
+          <div className="row d-flex justify-content-between p-0 m-0">
             <div className="col-6 align-self-end">
               <div id="userName" className="ms-2">
                 <h2 className="fs-4 m-0">Nombre de Usuario</h2>
                 <p className="fw-light m-0">@Nombredeusuario</p>
               </div>
             </div>
-            <div className="col-6 col-md-4 text-end align-self-end">
-              <div className="d-flex justify-content-between">
+            <div className="col-6 text-end align-self-end">
+              <div className="d-flex justify-content-end gap-2">
                 <a className="text-decoration-none text-black" href="#">
                   <p className="m-0 d-inline-block">
-                    <strong className="text-black">123</strong>
+                    <strong className="text-black">123 </strong>
                     Followers
                   </p>
                 </a>
                 <a className="text-decoration-none text-black" href="#">
                   <p className="m-0 d-inline-block">
-                    <strong className="text-black">123</strong>
+                    <strong className="text-black">123 </strong>
                     Following
                   </p>
                 </a>
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,19 +1,24 @@
 import React from "react";
 import ProfileHeader from "../components/ProfileHeader";
 import "./profile_styles.css";
+import Tweet from "../components/Tweet";
 
 function Profile() {
   return (
     <>
-      <container>
+      <div className="container">
         <div className="row">
-          <div className="col-2 col-lg-2"></div>
+          <div className="col-2 col-lg-2">Left Sidebar</div>
           <div className="col-8 col-lg-6 p-0">
             <ProfileHeader />
+            <div className="row position-relative border">
+              <Tweet />
+            </div>
           </div>
-          <div className="col-2 col-lg-4"></div>
+
+          <div className="col-2 col-lg-4">Right Sidebar</div>
         </div>
-      </container>
+      </div>
     </>
   );
 }
