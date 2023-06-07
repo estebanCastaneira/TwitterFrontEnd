b26e4c5dc499c5e751553163c79ef57dc05ecb87
andres-mps <andresmps@gmail.com>
2023-06-06 23:46:24 -0300

    FOLLOWING/ER a REACT

--- src/pages/Following.jsx
+++ src/pages/Following.jsx
@@ -1,33 +1,35 @@
 import React from "react";
+import "./Following.css";
+import { Link } from "react-router-dom";
 
 function Following() {
   return (
     <div className="container">
       <div className="row">
-        <div className="col-1 col-md-2">%- include("../partials/sidebar") %</div>
+        <div className="col-1 col-md-2">left_sidebar</div>
         <div className="col-11 col-md-6 col-lg-5">
           <div className="container-sm mw-50">
             <div className="d-flex flex-row align-items-center">
               <div className="mx-3">
-                <a className="text-black" href="/<%= locals.user.username %>">
-                  <i className="fas fa-arrow-left fa-lg"></i>
-                </a>
+                <Link className="text-black" to="/<%= locals.user.username %>">
+                  <i class="bi bi-arrow-left fs-4 fw-600"></i>
+                </Link>
               </div>
               <div>
-                <h1 className="mb-0 mt-3 fs-2">% user.firstname %%= user.lastname %</h1>
-                <small className="text-body-tertiary">% user.username%</small>
+                <h1 className="mb-0 mt-3 fs-2">Firstname Lastname</h1>
+                <small className="text-body-tertiary">Username</small>
               </div>
             </div>
             <div className="d-flex justify-content-between ml-2 my-3">
               <h3 className="fs-4">
-                <a className="text-black text-decoration-none" href="./followers">
+                <Link className="text-black text-decoration-none" to="./followers">
                   Followers
-                </a>
+                </Link>
               </h3>
               <h3 className="fs-4">
-                <a className="text-black text-decoration-none" href="./following">
+                <Link className="text-black text-decoration-none" to="./following">
                   Following
-                </a>
+                </Link>
               </h3>
             </div>
 
@@ -46,11 +48,9 @@ function Following() {
                 </div>
                 <div className="col-5">
                   <div className="card-body px-3">
-                    <h5 className="card-title mb-0 p-0 fs-6">
-                      %followingUser.firstname %% followingUser.lastname %
-                    </h5>
+                    <h5 className="card-title mb-0 p-0 fs-6">Firstname Lastname</h5>
                     <p className="card-text">
-                      <small className="text-body-tertiary">%followingUser.username%</small>
+                      <small className="text-body-tertiary">Username</small>
                     </p>
                   </div>
                 </div>
@@ -76,7 +76,7 @@ function Following() {
             {/* <!-- tarjeta FOLLOWERS --> */}
           </div>
         </div>
-        <div className="col-md-4">AGREGAR SIDEBAR DERECHO</div>
+        <div className="col-md-4">right sidebar</div>
       </div>
     </div>
   );
